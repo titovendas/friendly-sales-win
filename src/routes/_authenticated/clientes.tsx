a802ac33e837cc21a10967d0ad8b3d19761319e0
titovendas <titovendas1984@gmail.com>
2026-08-17 13:29:25 -0300

    Update div class for better styling

--- src/routes/_authenticated/clientes.tsx
+++ src/routes/_authenticated/clientes.tsx
@@ -231,7 +231,7 @@ function CustomersPage() {
         />
       </div>
 
-      <div className="rounded-md border">
+      <div className="overflow-x-auto rounded-md border">
         <Table>
           <TableHeader>
             <TableRow>
